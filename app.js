5f02ad84ccd28715b457e2defbe2fd125bdbd0c7
John Master <john.s.master@gmail.com>
2017-05-05 12:02:02 -0400

    in class

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-angular
-.module("stoplight", [])
-.controller("stoplightController", [stoplightControllerFunction])
+(function(){
+  'use strict'
 
-function stoplightControllerFunction(){
-  this.colors = ['red', 'yellow','green']
-  this.turn = function(color){
-    this.bg = color
+  angular
+  .module("stoplight", [
+    'ui.router'
+  ])
+  .config(['$stateProvider', RouterFunction])
+  .controller("stoplightController", ["$stateParams", "$state", stoplightControllerFunction])
+
+  function RouterFunction($stateProvider){
+    $stateProvider.state('color', {
+      url: '/:color',
+      controller: "stoplightController",
+      controllerAs: "vm",
+      templateUrl: "stoplight.html"
+    })
   }
-}
+  function stoplightControllerFunction($stateParams, $state){
+    console.log($state)
+    this.colors = ['red', 'yellow','green']
+    this.turn = function(color){
+      $state.go('color', {color: color})
+      this.bg = color
+    }
+    this.turn($stateParams.color)
+  }
+})()
