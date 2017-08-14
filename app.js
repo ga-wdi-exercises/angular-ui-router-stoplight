fadeba8cb07476c7b7ed378e73d3186369d8f292
Andrew Whitley <awhitley1233@gmail.com>
2017-08-14 11:31:59 -0400

    add solution

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-angular
-.module("stoplight", [])
-.controller("stoplightController", [stoplightControllerFunction])
+(function() {
 
-function stoplightControllerFunction(){
+angular
+.module("stoplight", [
+  'ui.router'
+])
+.config([
+  '$stateProvider',
+  RouterFunction
+])
+.controller("stoplightController", [
+  '$state',
+  '$stateParams',
+  stoplightControllerFunction
+])
+
+function RouterFunction ($stateProvider) {
+  $stateProvider
+    .state('color', {
+      url: '/:color',
+      controller: 'stoplightController',
+      controllerAs: 'vm',
+      templateUrl: 'stoplight.html'
+    })
+}
+
+function stoplightControllerFunction($state, $stateParams){
+  this.bg = $stateParams.color
   this.colors = ['red', 'yellow','green']
-  this.turn = function(color){
-    this.bg = color
+  this.turn = function(newColor){
+    $state.go('color', { color: newColor })
   }
 }
+
+
+})()
